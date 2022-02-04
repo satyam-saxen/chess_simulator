8abe920ddd9480a88031d5b3cdb7f40b5262bc92
Satyam Saxena <satyam@technogise.com>
2022-02-04 10:55:32 +0530

    Move board class to chess package

--- src/chess_simulator/Board.java
+++ src/chess_simulator/Board.java
@@ -1,2 +1,47 @@
+package chess_simulator;
+
 public class Board {
+  private char rowLowerRange;
+  private char rowHigherRange;
+  private char columnLowerRange;
+  private char columnHigherRange;
+
+  public Board(char rowLowerRange, char rowHigherRange, char columnLowerRange, char columnHigherRange) {
+    this.rowLowerRange = rowLowerRange;
+    this.rowHigherRange = rowHigherRange;
+    this.columnLowerRange = columnLowerRange;
+    this.columnHigherRange = columnHigherRange;
+  }
+
+  public char getRowLowerRange() {
+    return rowLowerRange;
+  }
+
+  public void setRowLowerRange(char rowLowerRange) {
+    this.rowLowerRange = rowLowerRange;
+  }
+
+  public char getRowHigherRange() {
+    return rowHigherRange;
+  }
+
+  public void setRowHigherRange(char rowHigherRange) {
+    this.rowHigherRange = rowHigherRange;
+  }
+
+  public char getColumnLowerRange() {
+    return columnLowerRange;
+  }
+
+  public void setColumnLowerRange(char columnLowerRange) {
+    this.columnLowerRange = columnLowerRange;
+  }
+
+  public char getColumnHigherRange() {
+    return columnHigherRange;
+  }
+
+  public void setColumnHigherRange(char columnHigherRange) {
+    this.columnHigherRange = columnHigherRange;
+  }
 }
