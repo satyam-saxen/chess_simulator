f371de12dcba7587c8c328a9332c72df1ec157ef
Satyam Saxena <satyam@technogise.com>
2022-02-04 11:50:50 +0530

    Add test for King moves

---
+++ src/test/com/chess/simulator/moves/KingMoveTest.java
@@ -0,0 +1,82 @@
+package test.com.chess.simulator.moves;
+
+import main.com.chess.simulator.Board;
+import main.com.chess.simulator.moves.KingMove;
+import main.com.chess.simulator.validators.Validation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static main.com.chess.simulator.validators.Validation.board;
+
+public class KingMoveTest {
+  @BeforeEach
+  void setup() {
+    Board board = new Board('A', 'H', '1', '8');
+    Validation.board = board;
+  }
+
+  @Test
+  public void testKingMove() {
+    char row = 'D';
+    char column = '4';
+
+    KingMove kingMove = new KingMove();
+    String response = kingMove.moves(row, column, board);
+
+    Assertions.assertEquals(9, response.split(",").length);
+  }
+
+  @Test
+  public void testKingMove_ifPositionIsAnyCorner() {
+    char minRow = 'A';
+    char minColumn = '1';
+    char maxRow = 'H';
+    char maxColumn = '8';
+
+    KingMove kingMove = new KingMove();
+    String LeftBottomPositionResponse = kingMove.moves(minRow, minColumn, board);
+    String RightBottomPositionResponse = kingMove.moves(minRow, maxColumn, board);
+    String LeftTopPositionResponse = kingMove.moves(minRow, maxColumn, board);
+    String RightTopPositionResponse = kingMove.moves(maxRow, maxColumn, board);
+
+    Assertions.assertEquals(4, LeftBottomPositionResponse.split(",").length);
+    Assertions.assertEquals(4, RightBottomPositionResponse.split(",").length);
+    Assertions.assertEquals(4, LeftTopPositionResponse.split(",").length);
+    Assertions.assertEquals(4, RightTopPositionResponse.split(",").length);
+  }
+
+  @Test
+  public void testKingMove_ifPositionIsAnyEdge() {
+    char minRow = 'A';
+    char minColumn = '2';
+    char maxRow = 'H';
+    char maxColumn = '7';
+
+    KingMove kingMove = new KingMove();
+    String LeftBottomPositionResponse = kingMove.moves(minRow, minColumn, board);
+    String RightBottomPositionResponse = kingMove.moves(minRow, maxColumn, board);
+    String LeftTopPositionResponse = kingMove.moves(minRow, maxColumn, board);
+    String RightTopPositionResponse = kingMove.moves(maxRow, maxColumn, board);
+
+    Assertions.assertEquals(6, LeftBottomPositionResponse.split(",").length);
+    Assertions.assertEquals(6, RightBottomPositionResponse.split(",").length);
+    Assertions.assertEquals(6, LeftTopPositionResponse.split(",").length);
+    Assertions.assertEquals(6, RightTopPositionResponse.split(",").length);
+
+    minRow = 'B';
+    minColumn = '1';
+    maxRow = 'G';
+    maxColumn = '8';
+
+    LeftBottomPositionResponse = kingMove.moves(minRow, minColumn, board);
+    RightBottomPositionResponse = kingMove.moves(minRow, maxColumn, board);
+    LeftTopPositionResponse = kingMove.moves(minRow, maxColumn, board);
+    RightTopPositionResponse = kingMove.moves(maxRow, maxColumn, board);
+
+    Assertions.assertEquals(6, LeftBottomPositionResponse.split(",").length);
+    Assertions.assertEquals(6, RightBottomPositionResponse.split(",").length);
+    Assertions.assertEquals(6, LeftTopPositionResponse.split(",").length);
+    Assertions.assertEquals(6, RightTopPositionResponse.split(",").length);
+  }
+}
