1a20a9e66961bc4489eb9a89f2b4f081bb589226
Satyam Saxena <satyam@technogise.com>
2022-02-04 13:03:55 +0530

    Remove unused line

--- src/main/com/chess/simulator/ChessSimulator.java
+++ src/main/com/chess/simulator/ChessSimulator.java
@@ -2,7 +2,6 @@ package main.com.chess.simulator;
 
 public class ChessSimulator {
   public static void main(String [] arg) {
-    System.out.println("Chess Simulator!");
     RunChess runChess = new RunChess();
     runChess.run();
   }
